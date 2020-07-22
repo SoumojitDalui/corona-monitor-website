aa207247ca6b6beab4f88784222488ac799a831b
Soumojit Dalui <39549918+SoumojitDalui@users.noreply.github.com>
2020-07-23 03:58:59 +0530

    Updates

---
+++ precache-manifest.a6b5a5e48e5e729cd23887cbcfc90596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bc00049962d2fca82a6b4d22644146",
+    "url": "/corona-monitor-website/index.html"
+  },
+  {
+    "revision": "63dc2189f96bf213c389",
+    "url": "/corona-monitor-website/static/css/main.0ca48b9c.chunk.css"
+  },
+  {
+    "revision": "416be4ee5ca533af874e",
+    "url": "/corona-monitor-website/static/js/2.2f53841a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-monitor-website/static/js/2.2f53841a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dc2189f96bf213c389",
+    "url": "/corona-monitor-website/static/js/main.45ea256c.chunk.js"
+  },
+  {
+    "revision": "09e436cfa1ce48ce4691",
+    "url": "/corona-monitor-website/static/js/runtime-main.8d9fecdc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-monitor-website/static/media/image.d7265326.png"
+  }
+]);
